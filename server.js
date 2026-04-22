70e0265b3774f35f22e3da5ff5bb4ba3484fdb72
m-tayyab-naseem <i222698@nu.edu.pk>
2026-04-22 15:06:42 +0500

    Fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 // 4. Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
